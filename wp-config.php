61b9152b4b6e9d45fdd6aa047c4cd9bc1c556275
Ajmal Afif <ajmalafif@gmail.com>
2013-12-17 03:09:05 +0800

    menu doesn't work

--- wp-config.php
+++ wp-config.php
@@ -20,8 +20,12 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 // ========================
 // Custom Content Directory
 // ========================
-define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp/');
-define( 'WP_INSTALL_DIR', dirname( __FILE__ ) .'/wp' );
+
+// define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp/');
+// define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/app' );
+// define( 'WP_INSTALL_DIR', dirname( __FILE__ ) .'/wp' );
+// define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/app' );
+
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/app' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/app' );
 
